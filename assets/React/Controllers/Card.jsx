7fb6b60020dc92319c0f7c7c08365307e6bc821d
RomeoFroger49 <romeo.froger@gmail.com>
2023-03-15 00:36:32 +0100

    add React first test component

---
+++ assets/React/Controllers/Card.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+export default function MyModal() {
+
+
+    return (
+        <>
+            <div className="w-full bg-white rounded shadow hover:shadow-lg"> salut</div>
+        </>
+    );
+}
